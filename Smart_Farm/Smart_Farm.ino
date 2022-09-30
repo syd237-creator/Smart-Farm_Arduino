453af58f76211e0c287ad20f568571d538e8a9a7
syd237-creator <63090113+syd237-creator@users.noreply.github.com>
2022-09-30 20:57:56 +0900

    Add files via upload

---
+++ Smart_Farm/Smart_Farm.ino
@@ -0,0 +1,294 @@
+#include <DHT.h>
+#include <SoftwareSerial.h>
+#define SIZE 15
+#define DHTPIN A0
+#define LX A1
+#define SOIL1 A2
+#define SOIL2 A3
+#define SOIL3 A4
+#define SOIL4 A5
+#define Relay_F 2
+#define Relay_L 3
+#define Relay_P1 4
+#define Relay_P2 5
+#define Relay_P3 6
+#define Relay_P4 7
+#define Humi_F 8
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+int Soil1_V;
+int Soil2_V;
+int Soil3_V;
+int Soil4_V;
+int Lx_V;
+
+int Buffer1_S[SIZE];
+int Buffer2_S[SIZE];
+int Buffer3_S[SIZE];
+int Buffer4_S[SIZE];
+float Sum1_S;
+float Sum2_S;
+float Sum3_S;
+float Sum4_S;
+int soil1;
+int soil2;
+int soil3;
+int soil4;
+
+int Buffer_L[SIZE];
+float Sum_L;
+int lx;
+
+float EMA_a = 0.4;
+int EMA_S1_pre = 0;
+int EMA_S1 = 0;
+int EMA_S2_pre = 0;
+int EMA_S2 = 0;
+int EMA_S3_pre = 0;
+int EMA_S3 = 0;
+int EMA_S4_pre = 0;
+int EMA_S4 = 0;
+
+int EMA_L_pre = 0;
+int EMA_L = 0;
+
+int h,t;
+
+int A = 0;
+int Fan_C = 0;
+int F = 0;
+int Fan_P = 0;
+
+int Humi_C = 0;
+int H = 0;
+int Humi_P = 0;
+
+int Lx_C = 0;
+int L = 0;
+int Lx_P = 0;
+
+int Pump1_C = 0;
+int P1 = 0;
+int Pump1_P = 0;
+
+int Pump2_C = 0;
+int P2 = 0;
+int Pump2_P = 0;
+
+int Pump3_C = 0;
+int P3 = 0;
+int Pump3_P = 0;
+
+int Pump4_C = 0;
+int P4 = 0;
+int Pump4_P = 0;
+
+int PA = 0;
+int b;
+unsigned long Pump1_N = 0;
+extern volatile unsigned long timer0_millis;
+
+void setup(){
+  pinMode(Relay_F, OUTPUT);
+  pinMode(Relay_L, OUTPUT);
+  pinMode(Relay_P1, OUTPUT);
+  pinMode(Relay_P2, OUTPUT);
+  pinMode(Relay_P3, OUTPUT);
+  pinMode(Relay_P4, OUTPUT);
+  pinMode(Humi_F,OUTPUT);
+  pinMode(SOIL1, INPUT);
+  pinMode(SOIL2, INPUT);
+  pinMode(SOIL3, INPUT);
+  pinMode(SOIL4, INPUT);
+  pinMode(LX, INPUT);
+  Serial.begin(9600);
+  Serial3.begin(9600);
+  EMA_S1_pre = analogRead(SOIL1);
+  EMA_S2_pre = analogRead(SOIL2);
+  EMA_S3_pre = analogRead(SOIL3);
+  EMA_S4_pre = analogRead(SOIL4);
+  EMA_L_pre = analogRead(LX);
+}
+
+void loop()
+{
+  Pump1_N = millis();
+  Serial.println(Pump1_N);
+  h = dht.readHumidity();
+  t = dht.readTemperature();
+  Com_Read();
+  Smart_F_A(A);
+  Soil1_F();
+  Soil2_F();
+  Soil3_F();
+  Soil4_F();
+  Lx_F();
+  Com_OUT();
+}
+void Com_OUT()
+{
+  Serial3.print("111");
+  Serial3.print(",");
+  Serial3.print(t);
+  Serial3.print(",");
+  Serial3.print(h);
+  Serial3.print(",");
+  Serial3.print(soil1);
+  Serial3.print(",");
+  Serial3.print(soil2);
+  Serial3.print(",");
+  Serial3.print(soil3);
+  Serial3.print(",");
+  Serial3.print(soil4);
+  Serial3.print(",");
+  Serial3.print(lx);
+  Serial3.print(",");
+  Serial3.print("222");
+  delay(50);
+}
+
+void Com_Read()
+{
+  if(Serial3.available())
+  {
+   char data = Serial3.read();
+   Serial.print(data);
+   if( data == 'a' )
+   {
+    F=1;
+   }
+   else if ( data == 'j' )
+   {
+    F=0;
+    Fan_C = 0;
+   }
+   else if( data == 'b' )
+   {
+    H=1;
+   }
+   else if( data == 'k' )
+   {
+    H=0;
+    Humi_C = 0;
+   }
+   else if( data == 'c' )
+   {
+    L=1;
+   }
+   else if( data == 'l' )
+   {
+    L=0;
+    Lx_C = 0;
+   }
+   else if( data == 'd' )
+   {
+    P1=1;
+    timer0_millis=0;
+   }
+   else if( data == 'm' )
+   {
+    P1=0;
+    Pump1_C = 0;
+   }
+   else if( data == 'e' )
+   {
+    P2=1;
+    timer0_millis=0;
+   }
+   else if( data == 'o' )
+   {
+    P2=0;
+    Pump2_C = 0;
+   }
+   else if( data == 'f' )
+   {
+    P3=1;
+    timer0_millis=0;
+   }
+   else if( data == 'p' )
+   {
+    P3=0;
+    Pump3_C = 0;
+   }
+   else if( data == 'g' )
+   {
+    P4=1;
+    timer0_millis=0;
+   }
+   else if( data == 'q' )
+   {
+    P4=0;
+    Pump4_C = 0;
+   }
+   else if( data == 'h' )
+   {
+    A=1;
+    b=1;
+   }
+   else if( data == 'i' )
+   {
+    A=0;
+   }
+  }
+}
+
+void Smart_F_A(int a)
+{
+  int N = millis();
+  if( a == 1 )
+  {
+    if( b == 1 )
+    {
+      if( t > 25 ) { F = 1; }
+      if( h < 40 ) { H = 1; }
+      if( soil1 < 40 ) { P1 = 1; }
+      if( soil1 < 40 ) { P1 = 1; }
+      if( soil2 < 40 ) { P2 = 1; }
+      if( soil3 < 40 ) { P3 = 1; }
+      if( soil4 < 40 ) { P4 = 1; }
+      b = 0;
+    }
+    if ( N - PA >= 1800000 )
+    {
+      PA = N;
+      if( t > 25 ) { F = 1; }
+      if( h < 40 ) { H = 1; }
+      if( soil1 < 40 ) { P1 = 1; }
+      if( soil1 < 40 ) { P1 = 1; }
+      if( soil2 < 40 ) { P2 = 1; }
+      if( soil3 < 40 ) { P3 = 1; }
+      if( soil4 < 40 ) { P4 = 1; }
+    }
+    if( lx < 250 ) { L = 1; }
+    
+    Fan(F);
+    Lx(L);
+    Humi(H);
+    Pump(P1,P2,P3,P4);
+
+    Fan_Count(10);
+    Humi_Count(10);
+    Lx_Count(10);
+    Pump1_Count(10);
+    Pump2_Count(10);
+    Pump3_Count(10);
+    Pump4_Count(10);
+  }
+  else if ( a == 0 )
+  {
+    Fan(F);
+    Lx(L);
+    Humi(H);
+    Pump(P1,P2,P3,P4);
+    
+   //Fan_Count(10);
+   //Humi_Count(10);
+   //Lx_Count(10);
+    Pump1_Count(10);
+    Pump2_Count(10);
+    Pump3_Count(10);
+    Pump4_Count(10);
+  }
+}
+
---
+++ Smart_Farm/group.ino
@@ -0,0 +1,329 @@
+void Fan(int a)
+{
+  if ( a == 1)
+  {
+    digitalWrite(Relay_F,HIGH);
+  }
+  else
+  {
+    digitalWrite(Relay_F,LOW);
+  }
+}
+
+void Lx(int a)
+{
+  if ( a == 1)
+  {
+    digitalWrite(Relay_L,HIGH);
+  }
+  else
+  {
+    digitalWrite(Relay_L,LOW);
+  }
+}
+
+void Humi(int a)
+{
+  if ( a == 1)
+  {
+    digitalWrite(Humi_F,HIGH);
+  }
+  else
+  {
+    digitalWrite(Humi_F,LOW);
+  }
+}
+
+void Pump(int a, int b, int c, int d)
+{
+  if ( a == 1)
+  {
+    digitalWrite(Relay_P1,HIGH);
+  }
+  else
+  {
+    digitalWrite(Relay_P1,LOW);
+  }
+  if ( b == 1)
+  {
+    digitalWrite(Relay_P2,HIGH);
+  }
+  else
+  {
+    digitalWrite(Relay_P2,LOW);
+  }
+  if ( c == 1)
+  {
+    digitalWrite(Relay_P3,HIGH);
+  }
+  else
+  {
+    digitalWrite(Relay_P3,LOW);
+  }
+  if ( d == 1)
+  {
+    digitalWrite(Relay_P4,HIGH);
+  }
+  else
+  {
+    digitalWrite(Relay_P4,LOW);
+  }
+}
+
+void Soil1_F()
+{
+  Soil1_V = analogRead(SOIL1);
+
+  EMA_S1_pre = (EMA_a*Soil1_V) + ((1-EMA_a)*EMA_S1_pre);
+  Sum1_S-=Buffer1_S[0];
+  for(int i = 0; i < SIZE-1; i++)
+  {
+    Buffer1_S[i] = Buffer1_S[i+1];
+  }
+  Buffer1_S[SIZE - 1] = EMA_S1_pre;
+  Sum1_S+=Buffer1_S[SIZE-1];
+  EMA_S1=Sum1_S/SIZE+2;
+
+  soil1 = map(EMA_S1,560,960,100,0);
+}
+
+void Soil2_F()
+{
+  Soil2_V = analogRead(SOIL2);
+
+  EMA_S2_pre = (EMA_a*Soil2_V) + ((1-EMA_a)*EMA_S2_pre);
+  Sum2_S-=Buffer2_S[0];
+  for(int i = 0; i < SIZE-1; i++)
+  {
+    Buffer2_S[i] = Buffer2_S[i+1];
+  }
+  Buffer2_S[SIZE - 1] = EMA_S2_pre;
+  Sum2_S+=Buffer2_S[SIZE-1];
+  EMA_S2=Sum2_S/SIZE+2;
+
+  soil2 = map(EMA_S2,560,960,100,0);
+}
+
+void Soil3_F()
+{
+  Soil3_V = analogRead(SOIL3);
+
+  EMA_S3_pre = (EMA_a*Soil3_V) + ((1-EMA_a)*EMA_S3_pre);
+  Sum3_S-=Buffer3_S[0];
+  for(int i = 0; i < SIZE-1; i++)
+  {
+    Buffer3_S[i] = Buffer3_S[i+1];
+  }
+  Buffer3_S[SIZE - 1] = EMA_S3_pre;
+  Sum3_S+=Buffer3_S[SIZE-1];
+  EMA_S3=Sum3_S/SIZE+2;
+
+  soil3 = map(EMA_S3,560,960,100,0);
+}
+
+void Soil4_F()
+{
+  Soil4_V = analogRead(SOIL4);
+
+  EMA_S4_pre = (EMA_a*Soil4_V) + ((1-EMA_a)*EMA_S4_pre);
+  Sum4_S-=Buffer4_S[0];
+  for(int i = 0; i < SIZE-1; i++)
+  {
+    Buffer4_S[i] = Buffer4_S[i+1];
+  }
+  Buffer4_S[SIZE - 1] = EMA_S4_pre;
+  Sum4_S+=Buffer4_S[SIZE-1];
+  EMA_S4=Sum4_S/SIZE+2;
+
+  soil4 = map(EMA_S4,560,960,100,0);
+}
+
+void Lx_F()
+{
+  Lx_V = analogRead(LX);
+
+  EMA_L_pre = (EMA_a*Lx_V) + ((1-EMA_a)*EMA_L_pre);
+  Sum_L-=Buffer_L[0];
+  for(int i = 0; i < SIZE-1; i++)
+  {
+    Buffer_L[i] = Buffer_L[i+1];
+  }
+  Buffer_L[SIZE - 1] = EMA_L_pre;
+  Sum_L+=Buffer_L[SIZE-1];
+  EMA_L=Sum_L/SIZE+2;
+
+  lx = map(EMA_L,0,1023,0,950);
+}
+
+void Fan_Count(int a)
+{
+  int Fan_N = millis();
+  if( F == 1)
+  {
+    if( Fan_N - Fan_P >= 1000 )
+    {
+      Fan_P = Fan_N;
+      Fan_C += 1;
+      Serial.println(Fan_C);
+      if(Fan_C == a)
+      {
+        Fan_C = 0;
+        F=0;
+        Serial3.print("333");
+        Serial3.print(",");
+        Serial3.print("a");
+        Serial3.print(",");
+        Serial3.print("444");
+        delay(100);
+      }
+    }
+  }
+}
+
+void Humi_Count(int a)
+{
+  int Humi_N = millis();
+  if( H == 1)
+  {
+    if( Humi_N - Humi_P >= 1000 )
+    {
+      Humi_P = Humi_N;
+      Humi_C += 1;
+      Serial.println(Humi_C);
+      if(Humi_C == a)
+      {
+        Humi_C = 0;
+        H=0;
+        Serial3.print("333");
+        Serial3.print(",");
+        Serial3.print("b");
+        Serial3.print(",");
+        Serial3.print("444");
+        delay(100);
+      }
+    }
+  }
+}
+
+void Lx_Count(int a)
+{
+  int Lx_N = millis();
+  if( L == 1)
+  {
+    if( Lx_N - Lx_P >= 1000 )
+    {
+      Lx_P = Lx_N;
+      Lx_C += 1;
+      Serial.println(Lx_C);
+      if(Lx_C == a)
+      {
+        Lx_C = 0;
+        L=0;
+        Serial3.print("333");
+        Serial3.print(",");
+        Serial3.print("c");
+        Serial3.print(",");
+        Serial3.print("444");
+        delay(100);
+      }
+    }
+  }
+}
+
+void Pump1_Count(int a)
+{
+  Pump1_N = millis();
+  
+  if( P1 == 1)
+  {
+    if( Pump1_N - Pump1_P >= 1000 )
+    {
+      Pump1_P = Pump1_N;
+      Pump1_C += 1;
+      Serial.println(Pump1_C);
+      if(Pump1_C == a)
+      {
+        Pump1_C = 0;
+        P1=0;
+        Serial3.print("333");
+        Serial3.print(",");
+        Serial3.print("d");
+        Serial3.print(",");
+        Serial3.print("444");
+        delay(100);
+      }
+    }
+  }
+}
+
+void Pump2_Count(int a)
+{
+  if( P2 == 1)
+  {
+    if( Pump1_N - Pump2_P >= 1000 )
+    {
+      Pump2_P = Pump1_N;
+      Pump2_C += 1;
+      Serial.println(Pump2_C);
+      if(Pump2_C == a)
+      {
+        Pump2_C = 0;
+        P2=0;
+        Serial3.print("333");
+        Serial3.print(",");
+        Serial3.print("e");
+        Serial3.print(",");
+        Serial3.print("444");
+        delay(100);
+      }
+    }
+  }
+}
+
+void Pump3_Count(int a)
+{
+  if( P3 == 1)
+  {
+    if( Pump1_N - Pump3_P >= 1000 )
+    {
+      Pump3_P = Pump1_N;
+      Pump3_C += 1;
+      Serial.println(Pump3_C);
+      if(Pump3_C == a)
+      {
+        Pump3_C = 0;
+        P3=0;
+        Serial3.print("333");
+        Serial3.print(",");
+        Serial3.print("f");
+        Serial3.print(",");
+        Serial3.print("444");
+        delay(100);
+      }
+    }
+  }
+}
+
+void Pump4_Count(int a)
+{
+  if( P4 == 1)
+  {
+    if( Pump1_N - Pump4_P >= 1000 )
+    {
+      Pump4_P = Pump1_N;
+      Pump4_C += 1;
+      Serial.println(Pump4_C);
+      if(Pump4_C == a)
+      {
+        Pump4_C = 0;
+        P4=0;
+        Serial3.print("333");
+        Serial3.print(",");
+        Serial3.print("g");
+        Serial3.print(",");
+        Serial3.print("444");
+        delay(100);
+      }
+    }
+  }
+}
